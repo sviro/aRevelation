0cc856e3e646814d8044aea7f213092b08b12568
MarmaladeSky <alexey.kislin@gmail.com>
2013-09-23 22:56:14 +0400

    add license and copyright info

--- src/com/github/marmalade/aRevelation/FileEntriesFragment.java
+++ src/com/github/marmalade/aRevelation/FileEntriesFragment.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2013 Aleksey Kislin
+ * Copyright 2013 Michal Švirec
+ *
+ * This file is part of aRevelation.
+ *
+ * aRevelation is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aRevelation is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aRevelation.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.github.marmalade.aRevelation;
 
 import android.app.*;
@@ -55,7 +74,6 @@ public class FileEntriesFragment extends Fragment implements
     private List<Entry> entries;
     private ArrayAdapter<Entry> entryArrayAdapter;
     private Activity activity;
-    //private Entry currentElement = null;
     
 
     public static FileEntriesFragment newInstance(String decryptedXML) {
@@ -113,7 +131,7 @@ public class FileEntriesFragment extends Fragment implements
             e.printStackTrace();
         }
         super.onStart();
-        // Set previous position
+        // Restore previous position
         lv.setSelectionFromTop(savedScrollBarPosition, top);
     }
 
